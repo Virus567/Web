e89a8c4abf11d2aef3954c1218c26186dd4a170e
Virus567 <ivan_bessonov_02@mail.ru>
2022-03-03 09:09:42 +0300

    sohranil

--- main/src/App.vue
+++ main/src/App.vue
@@ -2,20 +2,25 @@
   <div>
     <Fio lastName="Бессонов" firstName="Иван" middleName="Анатольевич"/>
     <Vyatsu :results="results"/>
-    <Registration/>
+    <div class="buttons">
+      <button v-on:click="toggleStyleLogin" class="login-btn">Войти</button>
+      <button v-on:click="toggleStyleRegister" class="register-btn">Зарегестрироваться</button>
+    </div>    
+    <Login class="login"/>
+    <Registration class="register"/>
   </div>
 </template>
-
 <script lang='ts'>
 import Vue from 'vue';
 import axios from 'axios';
 import Fio from '../../fio/src/components/Fio.vue';
 import Vyatsu from '../../vyatsu/src/components/Vyatsu.vue';
-import Registration from '../../login/src/components/login.vue';
+import Login from '../../login/src/components/login.vue';
+import Registration from '../../registration/src/components/registration.vue'
 
 const url = 'http://localhost:8080/admission/';
 
-export default {
+export default  Vue.extend({
   name: 'App',
   props: {
     results: [],
@@ -23,6 +28,7 @@ export default {
   components: {
     Fio,
     Vyatsu,
+    Login,
     Registration,
   },
   mounted() {
@@ -34,5 +40,65 @@ export default {
       })
       .catch((error: any) => console.log(error));
   },
-};
+  methods: {
+    toggleStyleLogin() {
+      const login :HTMLDivElement = document.querySelector('.login');
+      const register :HTMLDivElement = document.querySelector('.register');
+      login.style.display = 'flex';
+      register.style.display = 'none';
+    },
+    toggleStyleRegister() {
+      const login :HTMLDivElement = document.querySelector('.login');
+      const register :HTMLDivElement = document.querySelector('.register');
+      login.style.display = 'none';
+      register.style.display = 'flex';
+    },
+  },
+});
 </script>
+
+<style scoped>
+.main {
+width: 100%;
+}
+.register {
+display: none;
+}
+.buttons {
+display: flex;
+justify-content: center;
+width: 100%;
+margin: 10px 0;
+}
+.login-btn {
+background-color: green;
+margin-right: 10px;
+padding: 10px 100px;
+color: white;
+}
+.register-btn {
+background-color: green;
+margin-left: 10px;
+padding: 10px 55px;
+color: white;
+}
+@media (max-width:600px){
+  .buttons {
+  display: block;
+  justify-content: center;
+  width: 100%;
+  margin: 10px 0;
+  }
+  .login-btn {
+    background-color: green;
+    padding: 10px 100px;
+    color: white;
+  }
+.register-btn {
+background-color: green;
+padding: 10px 55px;
+color: white;
+}
+}
+</style>
+
---
+++ registration/src/components/registration.vue
@@ -0,0 +1,130 @@
+<template>
+<div class="container">
+  <div class="logo">
+    <img src="../assets/reg.png" alt="logo" class="biglogo-img">
+    <img src="../assets/slogo.png" alt="logo" class="logo-img">
+  </div>
+  <div class="registration_block">
+    <h1 class="title">Регистрация</h1>
+    <div class="input_desctop">
+      <div class="input-block">
+        <p>Логин</p>
+        <input type="text">
+      </div>
+      <div class="input-block">
+        <p>Пароль</p>
+        <input type="password">
+      </div>
+      <div class="input-block">
+        <p>Повторите пароль</p>
+        <input type="password">
+      </div>
+    </div>
+    <div class="input-mobil">
+      <input type="text" placeholder="Логин">
+      <input type="password" placeholder="Пароль">
+      <input type="password" placeholder="Повторите пароль">
+    </div>
+    <div>
+      <button type="button" class="Enter-btn">Зарегестрироваться</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+};
+</script>
+<style scoped>
+  .container{
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+  }
+  .logo{
+    margin-right: 10px;
+  }
+  .biglogo-img{
+    height: 280px;
+  }
+  .logo-img{
+    display: none;
+  }
+  .registration_block{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  .title{
+    margin-top: 0;
+    margin-bottom: 10px;
+  }
+  .Enter-btn{
+    background-color: green;
+  }
+  .save{
+    display: flex;
+    margin: 20px 0px 10px 0px;
+  }
+  .save-text{
+    margin: 0 auto;
+    margin-left: -1px;
+  }
+  .save input{
+    cursor: pointer;
+  }
+  .Enter-btn{
+    margin: 14px 0px;
+    padding: 14px 26px;
+    border-radius: 5px;
+    font-size: 16px;
+    color: white;
+    cursor: pointer;
+  }
+  .input-mobil{
+    display: none;
+    flex-direction: column;
+  }
+  .input-block p{
+      margin-bottom: 0px;
+  }
+  .input-block input{
+    font-size: 20px;
+    text-decoration: none;
+    padding: 2px 1px;
+    }
+  @media (max-width:600px){
+    .container{
+      display: block;
+    }
+    .biglogo-img{
+      display: none;
+    }
+    .logo-img{
+      display: block;
+      margin: 0 auto;
+      height: 100px;
+    }
+    .input_desctop{
+      display: none;
+    }
+    .input-mobil{
+      display: flex;
+    }
+    .input-mobil input{
+      margin-top: 10px;
+    }
+    .input-mobil input{
+    font-size: 20px;
+    text-decoration: none;
+    padding: 2px 1px;
+    }
+  }
+    @media (max-width:260px){
+      .input-mobil input{
+        width: 180px;
+    }
+    }
+</style>

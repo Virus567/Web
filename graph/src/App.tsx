157b380cb65a8c5dd5765eab56a30809667817e3
Virus567 <ivan_bessonov_02@mail.ru>
2022-04-11 20:03:43 +0300

    finish lab8

--- graph/src/App.tsx
+++ graph/src/App.tsx
@@ -4,7 +4,7 @@ import Graph from './components/Graph';
 function App() {
   return (
     <div className="App">
-      <Graph/>
+      <Graph min={0.000001} max={2.5} delta={0.001}/>
     </div>
   );
 }
--- graph/src/components/Graph.tsx
+++ graph/src/components/Graph.tsx
@@ -1,29 +1,99 @@
 import React from 'react';
-import * as d3 from 'd3';
+import * as am5 from "@amcharts/amcharts5";
+import * as am5xy from "@amcharts/amcharts5/xy";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 
-function funcMath(){
-  let funcArray : number[] = [];
-  let y : number;
-  for(let x = 0 ; x < 2.5; x += 0.1){
-    y = 3 * (x ** 3) - 2 ** (Math.E ** (x ** 3 - 2 * (x ** 2) - 1)) + (Math.log(x)/Math.log(13));
-    funcArray.push(y);
-  }   
-  return funcArray;  
+export declare type GraphProps = {
+	min: number,
+	max: number,
+	delta: number
 }
 
-function Graph() {
-    let data = [];
-    let yArr  = funcMath();
-    for(let i = 0; i < yArr.length; i += 0.1){
-      data.push({y : yArr[i*10], x : i });
-    }   
+type Point = {
+	x: number,
+	y: number
+}
 
-    return (
-      <div> 
-      <svg width={600} height={300}>
-      </svg>
-      </div>
-    );
-  }
+class Graph extends React.Component<GraphProps> {
+  funcMath() {
+		let points: Point[] = [];
+		let y: number;
+		for (let x = this.props.min; x < this.props.max; x += this.props.delta) {
+			y = 3 * (x ** 3) - 2 ** (Math.E ** (x ** 3 - 2 * (x ** 2) - 1)) + (Math.log(x) / Math.log(13));
+			points.push({x: x, y: y});
+		}
+		return points;
+	};
+
+
+  componentDidMount() {
+		const root = am5.Root.new("main");
+
+		root.setThemes([
+			am5themes_Animated.new(root)
+		]);
+
+		let data = this.funcMath();
+
+		let chart = root.container.children.push(
+			am5xy.XYChart.new(root, {
+				focusable: true,
+				panX: true,
+				panY: true,
+				wheelX: "panX",
+				wheelY: "zoomX"
+			})
+		);
+
+		let xAxis = chart.xAxes.push(
+			am5xy.ValueAxis.new(root, {
+					min: this.props.min,
+					max: this.props.max,
+					renderer: am5xy.AxisRendererX.new(root, {})
+				}
+			)
+		);
+
+		let yAxis = chart.yAxes.push(
+			am5xy.ValueAxis.new(root, {
+				maxDeviation: 0.1,
+				renderer: am5xy.AxisRendererY.new(root, {})
+			})
+		);
+
+		let series = chart.series.push(
+			am5xy.LineSeries.new(root, {
+				xAxis: xAxis,
+				yAxis: yAxis,
+				valueYField: "y",
+				valueXField: "x",
+				tooltip: am5.Tooltip.new(root, {
+					pointerOrientation: "horizontal",
+					labelText: "{valueY}"
+				})
+			})
+		);
+		series.data.setAll(data);
+
+		let legend = chart.children.push(am5.Legend.new(root, {}));
+		legend.data.setAll(chart.series.values);
+
+		let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+			xAxis: xAxis
+		}));
+		cursor.lineY.set("visible", false);
+
+		chart.set("scrollbarX", am5.Scrollbar.new(root, {
+			orientation: "horizontal"
+		}));
+	}
+
+	render() {
+		return <div>
+			<div id="main" style={{ width: "100%", height: "500px" }}/>
+		</div>;
+  };
+
+}
   
   export default Graph;

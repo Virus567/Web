0e04e112cbac86e30752c2f95d80834ec661dee2
Virus567 <ivan_bessonov_02@mail.ru>
2022-04-14 10:59:06 +0300

    add lab9

---
+++ WebServer/WebServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RestPanda;
+
+namespace WebServer;
+
+public class Program
+{
+    private static List<DateTime> _times = new();
+
+    public static int GetTimes()
+    {
+        _times.Add(DateTime.Now);
+        foreach (var time in _times.Where(time => (time - _times[^1]).Minutes >= 1))
+        {
+            _times.Remove(time);
+        }
+        return _times.Count;
+    }
+
+    public static void Main(string[] args)
+    {
+        var url = "http://localhost:8082/";
+        Console.WriteLine("Start server...");
+        var server = new PandaServer(url, typeof(Program));
+        server.Start();
+        Console.WriteLine("Server started at " + url);
+        Console.WriteLine("Press any key to stop server...");
+        Console.Read();
+        server.Stop();
+    }
+}
---
+++ WebServer/WebServer/Requests/HandlerGetStudent.cs
@@ -0,0 +1,34 @@
+﻿using RestPanda.Requests.Attributes;
+using RestPanda.Requests;
+using WebServer.Entity;
+namespace WebServer.Requests;
+
+[RequestHandler("/getstudent")]
+public class HandlerGetStudent
+{
+    [Get]
+    public static void GetStuds(PandaRequest request, PandaResponse response)
+    {
+        response.AddHeader("Time", DateTime.Now.ToString("O"));
+        response.AddHeader("Token", Guid.NewGuid().ToString());
+        response.AddHeader("Request-Per-Minutes", Program.GetTimes().ToString());
+        if (!request.Params.ContainsKey("id"))
+        {
+            response.Send(Student.Students);
+            return;
+        }
+        var f = request.Params["id"];
+        int start;
+        if (!int.TryParse(f, out start))
+        {
+            response.Send("Error");
+            return;
+        }
+        if (Student.Students.All(s => s.id != start))
+        {
+            response.Send("Error");
+            return;
+        }
+        response.Send(Student.Students.Single(s => s.id == start));
+    }
+}
